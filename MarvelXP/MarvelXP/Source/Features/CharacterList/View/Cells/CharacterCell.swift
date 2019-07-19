86a23f56b71a690fd01f58c06392315be1fdbd05
Roger Sanoli <rogersanoli.app@gmail.com>
2019-07-19 13:29:41 -0300

    Adding assets

---
+++ MarvelXP/MarvelXP/Source/Features/CharacterList/View/Cells/CharacterCell.swift
@@ -0,0 +1,27 @@
+//
+//  CharacterCell.swift
+//  MarvelXP
+//
+//  Created by Roger Sanoli on 19/07/19.
+//  Copyright © 2019 RogerSanoli. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CharacterCell: UICollectionViewCell {
+    
+    @IBOutlet weak private var characterImage: UIImageView!
+    @IBOutlet weak private var characterNameLabel: UILabel!
+    @IBOutlet weak private var favoriteButton: UIButton!
+    
+    private(set) var characterID: Int = 0
+    public var onFavorite: ((_ characterID: Int) -> Void)?
+    
+    public func setup(_ viewModel: CharacterViewModel) {
+        self.characterID = viewModel.id
+        self.characterImage.download(viewModel.photoULR, errorImage: "download_error")
+        self.characterNameLabel.text = viewModel.name
+        self.favoriteButton.isSelected = viewModel.isFavorite
+    }
+}
