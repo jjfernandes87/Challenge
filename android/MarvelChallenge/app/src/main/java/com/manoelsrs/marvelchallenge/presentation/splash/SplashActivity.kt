161100d9143582da91f4f915b5ab7ecda33b912c
Manoel Soares Neto <manoelsrs@hotmail.com>
2019-09-14 11:36:42 -0300

    Rename main activity to splash and organize into folder

---
+++ android/MarvelChallenge/app/src/main/java/com/manoelsrs/marvelchallenge/presentation/splash/SplashActivity.kt
@@ -0,0 +1,13 @@
+package com.manoelsrs.marvelchallenge.presentation.splash
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.manoelsrs.marvelchallenge.R
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
