f457d06b6829a052eea5eacbcb9e95076185e3fc
Manoel Soares Neto <manoelsrs@hotmail.com>
2019-09-17 21:04:05 -0300

    Delete fragment presenters

--- android/MarvelChallenge/app/src/main/java/com/manoelsrs/marvelchallenge/di/module/CharactersFragmentModule.kt
+++ android/MarvelChallenge/app/src/main/java/com/manoelsrs/marvelchallenge/di/module/CharactersFragmentModule.kt
@@ -2,22 +2,12 @@ package com.manoelsrs.marvelchallenge.di.module
 
 import com.manoelsrs.marvelchallenge.di.PerFragment
 import com.manoelsrs.marvelchallenge.presentation.home.characters.actions.GetCharacters
-import com.manoelsrs.marvelchallenge.presentation.home.characters.fragment.CharactersFragment
-import com.manoelsrs.marvelchallenge.presentation.home.characters.fragment.CharactersFragmentPresenter
 import com.manoelsrs.marvelchallenge.repository.Repository
 import dagger.Module
 import dagger.Provides
 
 @Module
 class CharactersFragmentModule {
-
-    @PerFragment
-    @Provides
-    fun providesCharactersFragmentPresenter(
-        fragment: CharactersFragment,
-        getCharacters: GetCharacters
-    ) =
-        CharactersFragmentPresenter(fragment, getCharacters)
 
     @PerFragment
     @Provides
