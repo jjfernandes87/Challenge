cbb417cbd802667b372d29b4c9939d093d001f17
Manoel Soares Neto <manoelsrs@hotmail.com>
2019-09-18 22:42:34 -0300

    Inject subject with dagger

--- android/MarvelChallenge/app/src/main/java/com/manoelsrs/marvelchallenge/di/module/CharactersActivityModule.kt
+++ android/MarvelChallenge/app/src/main/java/com/manoelsrs/marvelchallenge/di/module/CharactersActivityModule.kt
@@ -3,19 +3,23 @@ package com.manoelsrs.marvelchallenge.di.module
 import com.manoelsrs.marvelchallenge.di.PerActivity
 import com.manoelsrs.marvelchallenge.presentation.home.characters.CharactersActivity
 import com.manoelsrs.marvelchallenge.presentation.home.characters.CharactersPresenter
-import com.manoelsrs.marvelchallenge.presentation.home.characters.fragment.adapter.TabAdapter
 import com.manoelsrs.marvelchallenge.presentation.home.characters.fragment.CharactersFragment
 import com.manoelsrs.marvelchallenge.presentation.home.characters.fragment.FavoritesFragment
+import com.manoelsrs.marvelchallenge.presentation.home.characters.fragment.adapter.TabAdapter
 import dagger.Module
 import dagger.Provides
+import io.reactivex.subjects.BehaviorSubject
 
 @Module
 class CharactersActivityModule {
 
     @PerActivity
     @Provides
-    fun providesCharactersPresenter(activity: CharactersActivity) =
-        CharactersPresenter(activity)
+    fun providesCharactersPresenter(
+        activity: CharactersActivity,
+        subject: BehaviorSubject<String>
+    ) =
+        CharactersPresenter(activity, subject)
 
     @PerActivity
     @Provides
