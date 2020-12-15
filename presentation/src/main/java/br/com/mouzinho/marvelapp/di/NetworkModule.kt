327ffba5d73e92c56396ed6b9eb11aceb85a26fb
Luiz <luiz696silva@gmail.com>
2020-12-15 16:38:29 -0400

    Add caching on OkHttpClient to avoid exceeding api request limit

--- presentation/src/main/java/br/com/mouzinho/marvelapp/di/NetworkModule.kt
+++ presentation/src/main/java/br/com/mouzinho/marvelapp/di/NetworkModule.kt
@@ -1,21 +1,24 @@
 package br.com.mouzinho.marvelapp.di
 
+import android.content.Context
 import br.com.mouzinho.data.network.ApiService
 import br.com.mouzinho.marvelapp.di.MarvelQueryParams.Companion.PRIVATE_API_KEY_VALUE
 import br.com.mouzinho.marvelapp.di.MarvelQueryParams.Companion.PUBLIC_API_KEY_VALUE
+import br.com.mouzinho.marvelapp.util.hasInternetConnection
 import com.google.gson.Gson
 import com.google.gson.GsonBuilder
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ApplicationComponent
+import dagger.hilt.android.qualifiers.ApplicationContext
+import okhttp3.Cache
 import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
-import java.math.BigInteger
 import java.security.MessageDigest
 import javax.inject.Named
 import javax.inject.Singleton
@@ -29,6 +32,8 @@ object NetworkModule {
     private const val API_HASH_QUERY = "hash"
     private const val API_TIMESTAMP_QUERY = "ts"
     private const val NAMED_QUERY_PARAMS = "NAMED_QUERY_PARAMS"
+    private const val NAMED_API_KEY_INTERCEPTOR = "NAMED_API_KEY_INTERCEPTOR"
+    private const val NAMED_CACHE_INTERCEPTOR = "NAMED_CACHE_INTERCEPTOR"
 
     @Provides
     @Singleton
@@ -44,13 +49,20 @@ object NetworkModule {
         .build()
 
     @Provides
-    fun provideOkHttp(apiKeyInterceptor: Interceptor): OkHttpClient =
+    fun provideOkHttp(
+        @Named(NAMED_API_KEY_INTERCEPTOR) apiKeyInterceptor: Interceptor,
+        @Named(NAMED_CACHE_INTERCEPTOR) cacheInterceptor: Interceptor,
+        cache: Cache
+    ): OkHttpClient =
         OkHttpClient.Builder()
+            .cache(cache)
             .addInterceptor(apiKeyInterceptor)
+            .addInterceptor(cacheInterceptor)
             .addInterceptor(HttpLoggingInterceptor().apply { setLevel(HttpLoggingInterceptor.Level.BODY) })
             .build()
 
     @Provides
+    @Named(NAMED_API_KEY_INTERCEPTOR)
     fun providesApiKeyInterceptor(
         @Named(NAMED_QUERY_PARAMS) queryParams: MarvelQueryParams
     ) = Interceptor { chain ->
@@ -67,7 +79,7 @@ object NetworkModule {
     @Named(NAMED_QUERY_PARAMS)
     fun providesQueryParams(): MarvelQueryParams {
         val timeStamp = System.currentTimeMillis().toString()
-        val messageDigest =  MessageDigest.getInstance("MD5")
+        val messageDigest = MessageDigest.getInstance("MD5")
         messageDigest.update(timeStamp.toByteArray())
         messageDigest.update(PRIVATE_API_KEY_VALUE.toByteArray())
         messageDigest.update(PUBLIC_API_KEY_VALUE.toByteArray())
@@ -76,6 +88,21 @@ object NetworkModule {
     }
 
     @Provides
+    @Named(NAMED_CACHE_INTERCEPTOR)
+    fun providesCacheInterceptor(@ApplicationContext context: Context) = Interceptor { chain ->
+        var request = chain.request()
+        if (context.hasInternetConnection())
+            request = request.newBuilder().header("Cache-Control", "public, max-age=" + 120).build()
+        chain.proceed(request)
+    }
+
+    @Provides
     @Singleton
     fun provideGson(): Gson = GsonBuilder().serializeNulls().setDateFormat(API_DATE_FORMAT).create()
+
+    @Provides
+    fun providesCache(@ApplicationContext context: Context): Cache {
+        val cacheSize = (5 * 1024 * 1024).toLong()
+        return Cache(context.cacheDir, cacheSize)
+    }
 }
--- presentation/src/main/java/br/com/mouzinho/marvelapp/util/FragmentWithTitle.kt
+++
@@ -1,5 +0,0 @@
-package br.com.mouzinho.marvelapp.util
-
-interface FragmentWithTitle {
-    val title: String
-}
