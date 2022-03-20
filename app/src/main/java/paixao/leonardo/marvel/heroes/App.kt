c564c14bfb1beb584bcc3ee3180271c9385445f3
Leonardo Paixao <leonardorpaixao@gmail.com>
2022-03-20 13:31:28 -0300

    feat: icon name and label

--- app/src/main/java/paixao/leonardo/marvel/heroes/App.kt
+++ app/src/main/java/paixao/leonardo/marvel/heroes/App.kt
@@ -3,8 +3,8 @@ package paixao.leonardo.marvel.heroes
 import android.app.Application
 import org.kodein.di.DI
 import org.kodein.di.DIAware
+import paixao.leonardo.marvel.heroes.data.di.DataModule
 import paixao.leonardo.marvel.heroes.feature.network.di.NetworkingModule
-import paixao.leonardo.marvel.heroes.lib.di.DataModule
 
 class App : Application(), DIAware {
     override val di = DI {
