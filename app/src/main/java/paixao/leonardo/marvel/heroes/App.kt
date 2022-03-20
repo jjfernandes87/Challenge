6eea0de3b05587ad749bdbd6582f6b362b8561b9
Leonardo Paixao <leonardorpaixao@gmail.com>
2022-03-20 03:32:03 -0300

    feat: update DI of app module

--- app/src/main/java/paixao/leonardo/marvel/heroes/App.kt
+++ app/src/main/java/paixao/leonardo/marvel/heroes/App.kt
@@ -3,8 +3,12 @@ package paixao.leonardo.marvel.heroes
 import android.app.Application
 import org.kodein.di.DI
 import org.kodein.di.DIAware
+import paixao.leonardo.marvel.heroes.feature.network.di.NetworkingModule
+import paixao.leonardo.marvel.heroes.lib.di.DataModule
 
 class App : Application(), DIAware {
-    override val di: DI
-        get() = TODO("Not yet implemented")
+    override val di = DI {
+        import(NetworkingModule.injections)
+        import(DataModule.injections)
+    }
 }
