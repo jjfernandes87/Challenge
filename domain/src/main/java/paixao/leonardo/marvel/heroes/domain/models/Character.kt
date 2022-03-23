48466629e2e0607c95a00b5aa01263443dcbaa36
Leonardo Paixão <leonardorpaixao@gmail.com>
2022-03-23 02:49:04 -0300

    chore: adjusts to use room

--- domain/src/main/java/paixao/leonardo/marvel/heroes/domain/models/Character.kt
+++ domain/src/main/java/paixao/leonardo/marvel/heroes/domain/models/Character.kt
@@ -1,6 +1,8 @@
 package paixao.leonardo.marvel.heroes.domain.models
 
 data class Character(
+    val id: Int,
     val name: String,
+    val description: String?,
     val imageUrl: String
 )
--- domain/src/main/java/paixao/leonardo/marvel/heroes/domain/services/CharacterService.kt
+++ domain/src/main/java/paixao/leonardo/marvel/heroes/domain/services/CharacterService.kt
@@ -4,4 +4,10 @@ import paixao.leonardo.marvel.heroes.domain.models.Character
 
 interface CharacterService {
     suspend fun retrieveCharacter(): List<Character>
+
+    suspend fun retrieveFavoriteCharacters(): List<Character>
+
+    suspend fun saveFavoriteCharacter(character: Character): Unit
+
+    suspend fun removeFavoriteCharacter(character: Character): Unit
 }
